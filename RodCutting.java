1bdce02d844eee721176e0da2bae0369178d85a8
sarmad karatela <sarmadkaratela@sarmads-MacBook-Pro.local>
2019-04-02 21:10:10 -0400

    Rod Cutting

--- RodCutting.java
+++ RodCutting.java
@@ -1,16 +1,41 @@
 /**
  * Rod cutting problem described in Chapter 15 of textbook
+ * Sarmad Karatela
  */
 public class RodCutting {
 
   // Do not change the parameters!
   public int rodCuttingRecur(int rodLength, int[] lengthPrices) {
-    return 0;
+    //Base Case: if length is 0 then we just return the length 0
+    if (rodLength == 0){
+      return 0;
+    }
+    int result = -1;
   }
+    for (int i=0; i< rodLength; i++){
+      res = Math.max(res, lengthPrices[i-1] + rodCuttingRecur(rodLength - i, lengthPrices))
+    }
+    return res;
+}
 
   // Do not change the parameters!
   public int rodCuttingBottomUp(int rodLength, int[] lengthPrices) {
-    return 0;
+    int[] matrix = new int[rodLength + 1];
+
+    matrix[0] = 0;
+
+    for(int i=1; i<= rodLength; i++)
+  {
+    int result = -1;
+    for (int j = 0; j < i; j++)
+          { 
+    result = Math.max(result, lengthPrices[j] + matrix[i-(j + 1)]);
+      matrix[i] = res;
+          }
+    }
+  return matrix[rodLength];
+  }
+
   }
 
 
